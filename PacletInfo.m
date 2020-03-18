b01c1ab8a69fca7b0afbf29a1861657af41e41f2
Vasil Saroka <vasil.saroka@ntnu.no>
2020-03-18 19:05:43 +0300

    Package structure
    The basis package structure is specifed following CustomTicks, MaTeX and GTPack packages, and the test Mathematica notebook is added

---
+++ PacletInfo.m
@@ -0,0 +1,19 @@
+(* ::Package:: *)
+
+(* Paclet Info File *)
+
+(* created 2020/03/18 *)
+
+Paclet[
+    Name -> "TBpack",
+    Version -> "0.0.1",
+    MathematicaVersion -> "10.0+",
+    Description -> "Tight-binding calculations within Mathematica.",
+    Creator -> "Vasil Saroka <40.ovasil@gmail.com>",
+    URL -> "https://github.com/vasilsaroka/TBpack",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English", MainPage -> "Guides/TBpack"}, 
+            {"Kernel", Context -> "TBpack`"}
+        }
+]
